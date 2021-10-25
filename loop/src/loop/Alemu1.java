83a003bb0f06384eed116371e2830fed4e10130c
alemu robele <robele12@gmail.com>
2021-10-25 12:42:52 -0400

    ccc

---
+++ loop/src/loop/Alemu1.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class Alemu1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		int i=10;
+		for( i=0;i<=10;i++) {
+			System.out.println("the value of i is "+i);
+		}
+
+	}
+
+}
